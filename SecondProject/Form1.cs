e7aed65324e26849817472e8f12152d23d4df1cd
Микерин Илья Сергеевич <s20380424@unn.ru>
2021-03-12 13:36:17 +0300

    Добавьте файлы проекта.

---
+++ SecondProject/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SecondProject
+{
+    public partial class Form1 : Form
+    {
+        ImageList il1, il2; // Списки изображений
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graph = this.CreateGraphics();
+            il1.Draw(graph);
+            il2.Draw(graph);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (il1.isInside(e.X, e.Y))
+            {
+                il1.Select(true);
+                Invalidate();
+            }
+            else
+            {
+                il1.Select(false);
+                Invalidate();
+            }
+
+            if (il2.isInside(e.X, e.Y))
+            {
+                il2.Select(true);
+                Invalidate();
+            }
+            else
+            {
+                il2.Select(false);
+                Invalidate();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            il1 = new ImageList(0,0,20,20);
+            il2 = new ImageList(100, 100, 75, 98);
+        }
+    }
+}
---
+++ SecondProject/ImageList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+
+namespace SecondProject
+{
+    class ImageList
+    {
+        int X, Y; // Координаты
+        int W, H; // Размер
+        Color Line;// Цвет контура
+        Color Fill;// Цвет закраски
+        bool selected;// Состояние выделенности
+
+        public ImageList(int x,int y,int w,int h)
+        {
+            X = x;
+            Y = y;
+            W = w;
+            H = h;
+            Line = Color.Black;
+            Fill = Color.Red;
+        }
+
+        public void Draw(Graphics graph)
+        {
+            Pen pen = new Pen(Line);
+            if(selected == true)
+            {
+                pen.Color = Color.Green;
+            }
+            SolidBrush brush = new SolidBrush(Fill);
+
+            graph.FillRectangle(brush, X, Y, W, H);
+            graph.DrawRectangle(pen, X, Y, W, H);
+        }
+
+        public void Select(bool value)
+        {
+            selected = value;
+            //Перерисовать
+            
+        }
+
+        public bool isInside(int x, int y)
+        {
+            if(x<= X+W && x>= X && y>= Y && y<= Y + H)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
